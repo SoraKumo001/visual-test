ffc08ac0ab3c209422a9528e38635cc3378b5413
Akihiko Oikawa <akihiko.oikawa@finc.com>
2020-02-10 14:37:05 +0900

    no message

--- .github/scripts/comment.js
+++ .github/scripts/comment.js
@@ -12,7 +12,7 @@ const body = `
 Passed: ${jsonObject.passedItems.length}
 Faild: ${jsonObject.failedItems.length}
 New: ${jsonObject.newItems.length}
-Delete: ${jsonObject.deleteItems.length}
+Delete: ${jsonObject.deletedItems.length}
 `;
 
 request.post(
