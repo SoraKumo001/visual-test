b179b98e492de1b594730d3b1ba0c2a0a8334df0
Akihiko Oikawa <akihiko.oikawa@finc.com>
2020-02-10 15:52:59 +0900

    no message

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
-    <JSWindow>Test2</JSWindow>
+    <JSWindow>Test2-01</JSWindow>
     <div>Hello world!</div>
     <Link href="page02"><a>Page02</a></Link>
   </>
